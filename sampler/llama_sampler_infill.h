16d4dcc7e33207cc9722d79c7984311c21acbfc1
Kongfa Waroros <gongpha@hotmail.com>
2025-09-08 01:09:44 +0700

    remove unused line

--- sampler/llama_sampler_infill.h
+++ sampler/llama_sampler_infill.h
@@ -7,7 +7,6 @@ class LlamaSamplerInfill : public LlamaSampler {
 
 protected:
 	static void _bind_methods() {
-		ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "vocab", PROPERTY_HINT_RESOURCE_TYPE, "LlamaVocab"), "set_vocab", "get_vocab");
 	}
 	struct llama_sampler* _make_sampler(const llama_vocab *vocab) override {
 		ERR_FAIL_COND_V_MSG(vocab == nullptr, nullptr, "requries a valid LlamaVocab to create the sampler");
